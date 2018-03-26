b8540fca457e3ed0e22f53d7b30227fb4886de6a
protossw512 <helloxinyao.wang@gmail.com>
2018-03-25 22:55:38 -0700

    finish oauth flow

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+require('./models/User.js');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const keys = require('./config/keys');
+mongoose.connect(keys.mongoURI);
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-} );
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys:[keys.cookieKey]
+    })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
 app.listen(PORT);
